3fd18eb7bfcd3f5a45bc92ef0eedcb03627eeb96
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-07-25 22:24:01 +0600

    solved Question 3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const textReverse = (text) => {
     return reverseText;
   }
 };
-console.log(textReverse("hello world"), " => Question 1");
+// console.log(textReverse("hello world"), " => Question 1");
 
 // * Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
 // & Example Input: [2, -5, 10, -3, 7] Example Output: 19
@@ -24,4 +24,32 @@ const sumPositive = (arr) => {
     : "Please input a number of array";
   return result;
 };
-console.log(sumPositive([2, -5, 10, -3, 7]), " => Question 2");
+// console.log(sumPositive([2, -5, 10, -3, 7]), " => Question 2");
+
+// * Task 3: Write a JavaScript program to find the most frequent element in an array and return it.
+// & Example Input: [3, 5, 2, 5, 3, 3, 1, 4, 5] Example Output: 3
+
+const mostFrequent = (arr) => {
+  if (Array.isArray(arr)) {
+    const frequentObj = {};
+    arr.forEach((curr) => {
+      if (frequentObj[curr]) {
+        frequentObj[curr] += 1;
+      } else {
+        frequentObj[curr] = 1;
+      }
+    });
+    let maxFrequentKey = 0;
+    let maxFrequentValue = 0;
+    for (let i in frequentObj) {
+      if (maxFrequentValue < frequentObj[i]) {
+        maxFrequentKey = i;
+        maxFrequentValue = frequentObj[i];
+      }
+    }
+    return maxFrequentKey;
+  } else {
+    return "please input a valid number of array";
+  }
+};
+// console.log(mostFrequent([3, 5, 2, 5, 3, 3, 1, 4, 5]), " => Line No: 46");
