06888ce9860c1db77a7c6bd3e03904ef389b2d69
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-07-25 22:07:31 +0600

    solved Question 2

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ const textReverse = (text) => {
   }
 };
 console.log(textReverse("hello world"), " => Question 1");
+
+// * Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
+// & Example Input: [2, -5, 10, -3, 7] Example Output: 19
+const sumPositive = (arr) => {
+  console.log(Array.isArray(arr), " => Line No: 21");
+  const result = Array.isArray(arr)
+    ? arr.reduce((acu, curr) => {
+        return curr >= 0 ? acu + curr : acu + 0;
+      }, 0)
+    : "Please input a number of array";
+  return result;
+};
+console.log(sumPositive([2, -5, 10, -3, 7]), " => Question 2");
